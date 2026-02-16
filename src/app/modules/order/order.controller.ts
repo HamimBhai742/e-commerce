def905e2ae7dfcb05104870476d5b7dcc0509f74
HamimBhai742 <mdhamim5088@gmail.com>
2026-02-16 11:13:34 +0600

    update order controller

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -6,7 +6,7 @@ import httpStatus from "http-status";
 import { IJwtPayload } from "../../interface/user.interface";
 
 const createOrder=catchAsyncFn(async(req:Request &{user?:IJwtPayload},res:Response,next:NextFunction)=>{
-   const order=await orderServices.createOrder(req.user?.id as string,req.body.addressId)
+   const order=await orderServices.orderConfirmed(req.body.paymentStatus,req.user?.id as string,req.body.addressId,req.body.paymentId)
 
    sendResponse(res, { success:true, statusCode:httpStatus.CREATED, message:'Order created successfully', data:order})
 })
